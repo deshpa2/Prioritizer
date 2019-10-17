0a48a57afdf7205ad12eeb1a3650b84a8ef1ed77
mmagai <magaimahesh@gmail.com>
2019-10-17 01:31:27 -0400

    Prioritizer 2 updated

--- Assignment SE/src/util/Main.java
+++ Assignment SE/src/util/Main.java
@@ -4,134 +4,141 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static  <T> void main(String[] args) {
-		
-        Scanner sc = new Scanner(System.in);
-        T t = null;
-        
-        Prioritizer1<Object, Comparator> p1 = null;
-        //Prioritizer1<Object, Comparator> p2=new Prioritizer1<>();
-        //Prioritizer1<Object, Comparator> p3=new Prioritizer1<>();
-        
-        Object obj1 = null;
-        boolean flag=false;
-        do {
-        	System.out.println("Choose a prioritizer by entering 1,2 or 3");
-            int prioritizer = sc.nextInt(); 
-            System.out.println("Enter an operation from the following\n"
-            		+ "1:Insert elements\n"
-            		+ "2:Change Phase\n"
-            		+ "3:Remove Next Item\n"
-            		+ "4:Remove Any element\n"
-            		+ "5 Get Size\n"
-            		+ "6:Is In Insertion Phase?\n"
-            		+ "7:Exit");
-            int ch=sc.nextInt();
-        	switch(ch) {
-        	case 1:
-        		if(prioritizer==1) {
-        			System.out.println("Enter the size of the prioritizer");
-        			int size=sc.nextInt();
-        			
-        			p1=new Prioritizer1<>(size,t);
-        			for(int i=0;i<size;i++) {
-        				System.out.println("Enter "+i+1+" th Element");
-        				 obj1=sc.nextInt();
-        				 p1.insert((T)obj1,i);
-        			}
-        			T[] input=(T[]) p1.getInput();
-        			System.out.println("Input Received");
-        			//System.out.println(input);
-        			for (int i=0;i<input.length;i++) {
-        				System.out.println(input[i]);
-        			}
-        	break;		
-        	}
-        		if(prioritizer==2) {
-        			System.out.println("Enter elements to insert");
-        			//p2.insert((T)obj1,i);
-        	}
-        		if(prioritizer==3) {
-        			System.out.println("Enter elements to insert");
-        			 //obj1=sc.nextInt();
-        		   // p3.insert((T)obj1,i);
-        	}
-        	case 2:
-        		if(prioritizer==1) {
-         			p1.changePhase();
-        	}
-        		if(prioritizer==2) {
-        		  //p2.changePhase();
-        	}
-        		if(prioritizer==3) {
-        			//p3.changePhase();
-        	}
-        		break;
-        	case 3:
-        		if(prioritizer==1) {
-        			System.out.println("Removing Next Element");
-        			p1.removeNextInorder();
-        	}
-        		if(prioritizer==2) {
-        			System.out.println("Removing Next Element");
-        			//p2.removeNextInorder();
-        	}
-        		if(prioritizer==3) {
-        			System.out.println("Removing Next Element");
-        			//p3.removeNextInorder();
-        	}
-        		break;
-        	case 4:
-        		if(prioritizer==1) {
-        			System.out.println("Removing a random element");
-        			p1.removeAny();
-        	}
-        		if(prioritizer==2) {
-        			System.out.println("Removing a random element");
-        			//p2.removeAny();
-        	}
-        		if(prioritizer==3) {
-        			System.out.println("Removing a random element");
-        			//p3.removeAny();
-        	}
-        		break;
-        	case 5:
-        		if(prioritizer==1) {
-        			System.out.println("Getting size");
-        			int size=p1.getSize();
-        			System.out.println(size);
-        	}
-        		if(prioritizer==2) {
-        			System.out.println("Getting size");
-        			//p2.getSize();
-        	}
-        		if(prioritizer==3) {
-        			System.out.println("Getting size");
-        			//p3.getSize();
-        	}
-        		break;
-        	case 6:
-        		if(prioritizer==1) {
-        			System.out.println("Checking for insertion phase");
-        			boolean phase=p1.isInInsertionPhase();
-        			System.out.println(phase);
-        			
-        	}
-        		if(prioritizer==2) {
-        			System.out.println("Checking for insertion phase");
-        			//p2.isInInsertionPhase();
-        	}
-        		if(prioritizer==3) {
-        			System.out.println("Checking for insertion phase");
-        			//p3.isInInsertionPhase();
-        	}
-        	break;
-        	case 7:
-        		flag=true;
-        		break;
-        }
-        }while(flag==false);
-        System.out.print("Terminated");      		      
+
+	@SuppressWarnings({ "unchecked", "null" })
+	public static <T> void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		T t = null;
+
+		Prioritizer1<Object, Comparator> p1 = null;
+
+		// Prioritizer 2- Creating Object and Initialize it using Constructor
+		Prioritizer<T> p2 = null;
+
+		Object obj1 = null;
+		boolean flag = false;
+		do {
+			System.out.println("Choose a prioritizer by entering 1,2 or 3");
+			int prioritizer = sc.nextInt();
+			System.out.println("Enter an operation from the following\n" + "1:Insert elements\n" + "2:Change Phase\n"
+					+ "3:Remove Next Item\n" + "4:Remove Any element\n" + "5 Get Size\n" + "6:Is In Insertion Phase?\n"
+					+ "7:Exit");
+			int ch = sc.nextInt();
+			switch (ch) {
+			case 1:
+				if (prioritizer == 1) {
+					System.out.println("Enter the size of the prioritizer");
+					int size = sc.nextInt();
+
+					p1 = new Prioritizer1<>(size, t);
+					for (int i = 0; i < size; i++) {
+						System.out.println("Enter " + i + 1 + " th Element");
+						obj1 = sc.nextInt();
+						p1.insert((T) obj1, i);
+					}
+					T[] input = (T[]) p1.getInput();
+					System.out.println("Input Received");
+					// System.out.println(input);
+					for (int i = 0; i < input.length; i++) {
+						System.out.println(input[i]);
+					}
+					break;
+				}
+				if (prioritizer == 2) {
+					System.out.println("Enter the size of the prioritizer");
+					@SuppressWarnings("resource")
+					Scanner sc1 = new Scanner(System.in);
+					int a = sc1.nextInt();
+					Object size = a;
+					p2 = new Prioritizer2<T>(a);
+					p2.insert((T) size, 0);
+					break;
+				}
+				if (prioritizer == 3) {
+					System.out.println("Enter elements to insert");
+					// obj1=sc.nextInt();
+					// p3.insert((T)obj1,i);
+				}
+			case 2:
+				if (prioritizer == 1) {
+					p1.changePhase();
+				}
+				if (prioritizer == 2) {
+					p2.changePhase();
+				}
+				if (prioritizer == 3) {
+					// p3.changePhase();
+				}
+				break;
+			case 3:
+				if (prioritizer == 1) {
+					System.out.println("Removing Next Element");
+					p1.removeNextInorder();
+				}
+				if (prioritizer == 2) {
+					System.out.println("Removing Next Element");
+					p2.removeNextInorder();
+				}
+				if (prioritizer == 3) {
+					System.out.println("Removing Next Element");
+					// p3.removeNextInorder();
+				}
+				break;
+			case 4:
+				if (prioritizer == 1) {
+					System.out.println("Removing a random element");
+					p1.removeAny();
+				}
+				if (prioritizer == 2) {
+					System.out.println("Removing a random element");
+					p2.removeAny();
+				}
+				if (prioritizer == 3) {
+					System.out.println("Removing a random element");
+					// p3.removeAny();
+				}
+				break;
+			case 5:
+				if (prioritizer == 1) {
+					System.out.println("Getting size");
+					int size = p1.getSize();
+					System.out.println(size);
+				}
+				if (prioritizer == 2) {
+					int size = p2.getSize();
+					System.out.println("Size of the array =  " + size);
+				}
+				if (prioritizer == 3) {
+					System.out.println("Getting size");
+					// p3.getSize();
+				}
+				break;
+			case 6:
+				if (prioritizer == 1) {
+					System.out.println("Checking for insertion phase");
+					boolean phase = p1.isInInsertionPhase();
+					System.out.println(phase);
+
+				}
+				if (prioritizer == 2) {
+					System.out.println("Checking for insertion phase");
+					if (p2.isInInsertionPhase() == true) {
+						System.out.println("IsInInsertionPhase");
+					} else {
+						System.out.println("IsInRemovalPhase");
+					}
+				}
+				if (prioritizer == 3) {
+					System.out.println("Checking for insertion phase");
+					// p3.isInInsertionPhase();
+				}
+				break;
+			case 7:
+				flag = true;
+				break;
+			}
+		} while (flag == false);
+		System.out.print("Terminated");
 	}
 }
---
+++ Assignment SE/src/util/Prioritizer2.java
@@ -0,0 +1,166 @@
+package util;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Prioritizer2<T> implements Prioritizer<T> {
+
+	private int top = 0;
+	private T contents[];
+	private boolean flag;
+	private int x = 0;
+	private T newContents[];
+	int index=0;
+
+	@SuppressWarnings("unchecked")
+	public Prioritizer2(int i) {
+		// TODO Auto-generated constructor stub
+
+		contents = (T[]) new Object[i];
+	}
+
+	public int getTop() {
+		return top;
+	}
+
+	public void setTop(int top) {
+		this.top = top;
+	}
+
+	public T[] getContents() {
+		return contents;
+	}
+
+	public void setContents(T[] contents) {
+		this.contents = contents;
+	}
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public T[] getNewContents() {
+		return newContents;
+	}
+
+	public void setNewContents(T[] newContents) {
+		this.newContents = newContents;
+	}
+
+	
+
+	@Override
+	public int compare(T o1, T o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void insert(T x, int a) {
+		 flag=true;
+		// TODO Auto-generated method stub
+		int r = (int) x;
+		for (int i = 0; i < r; i++) {
+			System.out.println("Enter " + i + "th Element");
+			@SuppressWarnings("resource")
+			Scanner myObj = new Scanner(System.in);
+			Object c = (Object) myObj.nextInt();
+			contents[top++] = (T) c;
+		}
+		Arrays.sort(contents);
+		for (int i = 0; i < contents.length; i++) {
+			System.out.println(contents[i]);
+		}
+	}
+
+	@Override
+	public void changePhase() {
+		// TODO Auto-generated method stub
+		if(flag) {
+			flag=false;
+			System.out.println("System Phase changed to Removal");
+		}
+		else {
+			flag=true;
+			System.out.println("System Phase changed to Insertion");
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void removeNextInorder() {
+		// TODO Auto-generated method stub
+		 flag=false;
+		 x=contents.length-1;
+		for (int i = 0; i < contents.length; i++) {
+			if (i == x) {
+				newContents = (T[]) new Object[contents.length - 1];
+				for (int j = 0; j < i; j++) {
+					newContents[j] = contents[j];
+				}
+				for (int k = i; k < contents.length - 1; k++) {
+					newContents[k] = contents[k + 1];
+				}
+				break;
+			}
+		}
+
+		System.out.println("After removing index" + x);
+		x++;
+
+		for (int i = 0; i < newContents.length; i++) {
+			System.out.println(newContents[i]);
+		}
+		top=newContents.length;
+	}
+
+	@Override
+	public void removeAny() {
+		// TODO Auto-generated method stub
+		index=contents.length-1;
+		 flag=false;
+		@SuppressWarnings("unchecked")
+		T[] newArray = (T[]) new Object[contents.length - 1];
+       
+		for (int i = 0, k = 0; i < contents.length; i++) {
+			if (i == index) {
+				continue;
+			}
+			newArray[k++] = contents[i];
+		}
+
+		System.out.println("After Removing element");
+		for (int i = 0; i < newArray.length; i++) {
+			System.out.println(newArray[i]);
+		}
+		top=newArray.length;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return top;
+	}
+
+	@Override
+	public boolean isInInsertionPhase() {
+		// TODO Auto-generated method stub
+		return flag;
+
+	}
+
+}
